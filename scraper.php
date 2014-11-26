4326af09d366756efdc5f11ef6b536a3040b4bce
obviousninja <infinityarc@gmail.com>
2014-11-25 22:32:38 -0500

    productcategory complete

--- scraper.php
+++ scraper.php
@@ -238,59 +238,89 @@
        //reserved for code populating subcategories
        //, $type[0][0] are the subtypes
        //fruit subcategory
-       $fruitsubsite=array(); //containing site address for the fruit
+       
+       
+       
+       $subsubsite=array(); //containing site address for the fruit
        for($x=15; $x<=28; $x++){
         preg_match_all($patterntype, $collections[0][$x], $type); //getting te name of the type, it will be under $type[0][0] #NOT THE SUBSITE ADDRESS
         //name of the subsite address is $collections[0][$x]
         //create an associate array with name of the type and it points to the subsite address
+        $subsubsite[$type[0][0]] = $collections[0][$x];
          
         //array_push($typeArr['fruit'], $type[0][0]);
-        array_push($typeArr['fruit'], array());
-        $typeArr['fruit'] = ""
+        //array_push($typeArr['fruit'], array());
+        //$typeArr['fruit'] = ""
         //array_push($typeArr['fruit'])
        }
-        print_r($typeArr);
+       array_push($typeArr['fruit'], $subsubsite); //populate fruit and its subtype
+       
+       
        //veggies subcategory
+       $subsubsite = array();
        for($x=31; $x<=48; $x++){
         preg_match_all($patterntype, $collections[0][$x], $type);
       
-        array_push($typeArr['vegies'], $type[0][0]);
+         $subsubsite[$type[0][0]] = $collections[0][$x];
        }
-     
+      array_push($typeArr['vegies'], $subsubsite); //populate vegies and its subtypes
+       //print_r($typeArr);
        //groceries subcategory
+       $subsubsite = array();
        for($x=51; $x<=70; $x++){
-         preg_match_all($patterntype, $collections[0][$x], $type);
-        
-        array_push($typeArr['groceries'], $type[0][0]);
+        preg_match_all($patterntype, $collections[0][$x], $type);
+      
+         $subsubsite[$type[0][0]] = $collections[0][$x];
        }
+             array_push($typeArr['groceries'], $subsubsite); //groceries population
        //fridge subcategory
+              $subsubsite = array();
        for($x=73; $x<=84; $x++){
-           preg_match_all($patterntype, $collections[0][$x], $type);
-          //  echo "<p>i am a monster below</p>";
-       // print_r($type);
-        array_push($typeArr['fridge'], $type[0][0]);
+                   preg_match_all($patterntype, $collections[0][$x], $type);
+      
+         $subsubsite[$type[0][0]] = $collections[0][$x];
        }
+       array_push($typeArr['fridge'], $subsubsite); //fridges
         //butcher subcategory
+               $subsubsite = array();
         for($x=87; $x<=96; $x++){
-           preg_match_all($patterntype, $collections[0][$x], $type);
-        array_push($typeArr['butcher'], $type[0][0]);
+        preg_match_all($patterntype, $collections[0][$x], $type);
+      
+         $subsubsite[$type[0][0]] = $collections[0][$x];
        }
+       array_push($typeArr['butcher'], $subsubsite); //
         //seafood subcategory
+               $subsubsite = array();
         for($x=99; $x<=107; $x++){
-         preg_match_all($patterntype, $collections[0][$x], $type);
-        array_push($typeArr['seafood'], $type[0][0]);
+        preg_match_all($patterntype, $collections[0][$x], $type);
+      
+         $subsubsite[$type[0][0]] = $collections[0][$x];
        }
+       array_push($typeArr['seafood'], $subsubsite); //
         //organic subcategory
+               $subsubsite = array();
          for($x=109; $x<=113; $x++){
-         preg_match_all($patterntype, $collections[0][$x], $type);
-        array_push($typeArr['organic'], $type[0][0]);
+        preg_match_all($patterntype, $collections[0][$x], $type);
+      
+         $subsubsite[$type[0][0]] = $collections[0][$x];
        }
+       array_push($typeArr['organic'], $subsubsite); //
         //wholesale subcategory
+               $subsubsite = array();
         for($x=116; $x<=120; $x++){
-         preg_match_all($patterntype, $collections[0][$x], $type);
-        array_push($typeArr['seafood'], $type[0][0]);
+        preg_match_all($patterntype, $collections[0][$x], $type);
+      
+         $subsubsite[$type[0][0]] = $collections[0][$x];
        }
+    array_push($typeArr['wholesale'], $subsubsite); //
 
+    //print_r($typeArr); //printing the filled associative array
+    insertProductTypes($typeArr);  //populate the sql database PRODUCTCATEGORY, THIS IS THE RIGHT CODE
+    
+    //scanProducts('http://www.harrisfarm.com.au/collections/apples'); //only scan one page
+    
+    
+    
        //geting nav content
      //  $patternnav = '/<nav.*<\/nav>/i';
     //   preg_match_all($patternnav, $subject, $navProc);
@@ -305,13 +335,89 @@
 
 
     }
-    
+    //
+    function scanProducts($webAddress){
+         $currentAddress = file_get_contents($webAddress);
+         
+         echo htmlspecialchars($currentAddress);
+         /*
+           <div class="imgcont loadingimg">
+       
+            <a href="/collections/asparagus-broccoli-cauliflower/products/asparagus" class="image-inner-wrap ">
+				
+				
+			  <img src="//cdn.shopify.com/s/files/1/0206/9470/products/asparagus_1_093193e3-a081-4e35-93c6-2a2be62730d5_compact.jpeg?v=1409621749" alt="Asparagus (bunch)" />
+			 
+              <div class="salecontainer">
+				<span class="sale">			
+					<small>from</small> $1.69
+    				
+    				
+				</span>
+			  </div>
+            </a>
+    </div>
+    <p class="title"><a href="/collections/asparagus-broccoli-cauliflower/products/asparagus">Asparagus (bunch)</a></p>
+          */   ##pattern on each page
+         
+         
+         
+    }
+    function insertProductTypes($associativeArrayArray){
+        //open connection
+           $servername = "localhost";
+        $username = "jchen127";
+        $password = "KbZFqBcZCy29b3Lx";
+        $dbname = "mydb";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        //construct query
+        //echo "<p>array keys<p>";
+       // print_r(array_keys($associativeArrayArray['fruit'][0])); //sub fruit array starts from ['fruit'][0]
+       // category varchar(40) NOT NULL,
+        //subcategory varchar(40) not null,
+        //$insertion = "INSERT INTO productcategory (category, subcategory) values ('sick', 'monster')";
+        
+        foreach($associativeArrayArray as $x => $x_value) { //grand type
+          //construct query string
+          foreach($associativeArrayArray[$x][0] as $y => $y_value){ //subtype $y is the key
+             $insertion = "INSERT INTO productcategory (category, subcategory) values ('$x', '$y')";
+             
+            if ($conn->query($insertion) === TRUE) {
+             echo "New record created successfully";
+             } else {
+              echo "Error: " . $insertion . "<br>" . $conn->error;
+              echo "<br>";
+             }
+          }
+        
+        }
+     
+     
+     //   if ($conn->query($insertion) === TRUE) {
+     //echo "New record created successfully";
+    //} else {
+     // echo "Error: " . $sql . "<br>" . $conn->error;
+   // }
+        
+        
+        
+        
+        
+        
+        //close the connection
+        $conn->close();
+    }
 
     
     function estabConnect(){
         //main
-       // createDatabase();
-       // createTables();
+        //createDatabase();
+        //createTables();
         //populate database
         scrapeMasterMarkK();
         
